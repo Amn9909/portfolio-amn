18949f18ebd520e182976076623064a13aab62aa
aman <118736234+amnak09@users.noreply.github.com>
2024-12-08 13:23:39 +0530

    added test comps

---
+++ app/trial/page.tsx
@@ -0,0 +1,14 @@
+import { SVGMaskEffectDemo } from '@/components/acernity/Focus'
+import { SparklesPreview } from '@/components/acernity/Sparcles'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        {/* <SparklesPreview /> */}
+        <SVGMaskEffectDemo />
+    </div>
+  )
+}
+
+export default page
